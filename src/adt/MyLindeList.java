848970666aba849f113627cd973dd4641048e7c1
csj <csj@lylq>
2018-01-03 18:42:41 +0800

    第一次提交

---
+++ src/adt/MyLindeList.java
@@ -0,0 +1,15 @@
+package adt;
+
+public class MyLindeList {
+	private Node begin;
+	
+	private Node length;
+	
+	
+	
+	
+}
+
+class Node {
+	private Node pre;
+}
---
+++ src/com/test.java
@@ -0,0 +1,13 @@
+package com;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//第一次测试 
+		System.out.print("第一行代码");
+		System.out.print("伤心了一批");
+	}
+
+
+}
